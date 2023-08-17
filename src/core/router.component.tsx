fe99a0cc572e57b651ed49a3c0c0a5c3036df409
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-08-17 19:44:54 +0200

    installed ghpages library to solve deployment issues

--- src/core/router.component.tsx
+++ src/core/router.component.tsx
@@ -5,14 +5,13 @@ import { ListPage } from "../components/list";
 import { DetailPage } from "../components/detail";
 
 export const RouterComponent = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-        <Route path="*" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/list" element={<ListPage />} />
+                <Route path="/detail/:id" element={<DetailPage />} />
+            </Routes>
+        </Router>
+    );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
-import "./styles.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
